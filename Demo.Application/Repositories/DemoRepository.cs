e17960e1b86e7480dab872b17e5feebbe343a6be
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-14 17:51:18 +0500

    Repository

--- Demo.Application/Repositories/DemoRepository.cs
+++ Demo.Application/Repositories/DemoRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Demo.Application.DataAccess;
-using Demo.Domain.Models;
+using Demo.Domain.BaseModels.Enums;
+using Demo.Domain.DTOs.UserDTOs;
+using Demo.Domain.Models.UserModels;
 using Microsoft.EntityFrameworkCore;
 
 namespace Demo.Application.Repositories
@@ -12,10 +14,47 @@ namespace Demo.Application.Repositories
             _dbContext = context;
             
         }
+
+        public async ValueTask<bool> CreateAsync(UserDto userDto)
+        {
+            try
+            {
+                User user = new User()
+                {
+                    Name = userDto.Name,
+                    Email = userDto.Email,
+                    Age = userDto.Age,
+                    Password = userDto.Password,
+                };
+                await _dbContext.Users.AddAsync(user);
+                int x=await _dbContext.SaveChangesAsync();
+                return x>0;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public ValueTask<bool> DeleteAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
         public async ValueTask<IEnumerable<User>> GetAllUsersAsync()
         {
             IEnumerable<User> users=await _dbContext.Users.ToListAsync();
             return users;
+        }
+
+        public ValueTask<User> GetByIdAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<bool> UpdateAsync(UpdateDto updateDto)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Demo.Application/Repositories/IDemoRepository.cs
+++ Demo.Application/Repositories/IDemoRepository.cs
@@ -1,9 +1,14 @@
-﻿using Demo.Domain.Models;
+﻿using Demo.Domain.DTOs.UserDTOs;
+using Demo.Domain.Models.UserModels;
 
 namespace Demo.Application.Repositories
 {
     public interface IDemoRepository
     {
         public ValueTask<IEnumerable<User>> GetAllUsersAsync();
+        public ValueTask<User> GetByIdAsync();
+        public ValueTask<bool> CreateAsync(UserDto userDto);
+        public ValueTask<bool> UpdateAsync(UpdateDto updateDto);
+        public ValueTask<bool> DeleteAsync(int Id);
     }
 }
