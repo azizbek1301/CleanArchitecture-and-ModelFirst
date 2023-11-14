0be6a9eb9461ca87e6dbcb048e4872188706d732
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-14 17:51:58 +0500

    add dto

---
+++ Demo.Domain/DTOs/UserDTOs/UpdateDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Demo.Domain.DTOs.UserDTOs
+{
+    public class UpdateDto
+    {
+        public int Id { get; set; } 
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        
+        public string Password { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Demo.Domain/DTOs/UserDTOs/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Domain.DTOs.UserDTOs
+{
+    public class UserDto
+    {
+        [Required]
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public int Age { get; set; }
+    }
+}
